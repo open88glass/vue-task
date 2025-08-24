135d9191c079dc2f50b934935a84930de2cd47fe
open88glass <open88glass@gmail.com>
2025-08-24 14:02:58 -0300

    fix: fix vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
   plugins: [vue()],
   test: {
     environment: "jsdom",
